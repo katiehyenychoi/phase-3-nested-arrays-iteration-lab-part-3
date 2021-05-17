0d2acc0e8eac10fd06515872cbeba08206b0e36e
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-05-17 17:23:00 -0400

    commit tree fd5ebec0204a2f4fa88640e7aedd29009417132e

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,30 @@
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+
+  count=0
+  result=""
+
+  while count < src.count do
+  innercount = 0
+
+  while innercount < src[count].count
+  if src[count][innercount].class == String 
+    result += src[count][innercount] + ' '
+  end
+  innercount += 1
+  end
+  count += 1
+ end
+result
 end
+
+mixed_data = [
+  ["The", 4, "quick"],
+  [-1, "brown", "fox", 30],
+  ["studied", 101, 233, "Ruby"]
+]
+
+
+join_nested_strings(mixed_data)
+
